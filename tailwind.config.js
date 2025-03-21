f9e510bdf441c150359db9113a79a9995798ff9c
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-03-21 17:04:19 +0700

    add custom font families to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        delius : ['Delius Unicase', 'cursive'],
+        deliusSwash : ['Delius Swash Caps', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
